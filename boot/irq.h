60b19954fe804e5017e665ecf3afb1a320bbd183
Yggdrasill <yggdrasill@shitpost.is>
2017-10-15 20:22:14 +0200

    Add IRQ-related functions, add file irq.c irq.h
            This commit adds the files:
    
            irq.h
            irq.c
    
            And the functions:
    
            void irq_init(void):
            uint16_t irq_read_reg(unsigned char reg);
            void irq_mask(unsigned char irq);
            void irq_unmask(unsigned char irq);
            void irq_mask_all(void);
            void irq_unmask_all(void);
    
            irq_init() sets up the two PIC 8259 chips to:
    
            - Remap IRQs to 32-48
            - Use master/slave mode
            - Use 8086 mode
    
            irq_read_reg() *should* read the IRR or ISR depending on the
            register specified. This is untested. It returns a number larger
            than the highest IRQ on failure, so it returns >0x0F.
    
            irq_mask() masks the specified IRQ.
    
            irq_unmask() unmasks the specified IRQ.
    
            irq_mask_all() masks all IRQs.
    
            irq_unmask_all() unmasks all IRQs.

---
+++ boot/irq.h
@@ -0,0 +1,34 @@
+/*
+ * MBR bootloader, currently unnamed
+ * Copyright (C) 2017  Yggdrasill <kaymeerah@lambda.is>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ */
+
+#ifndef IRQ_H
+#define IRQ_H
+
+#include "idt.h"
+#include "interrupt.h"
+
+void irq_init(void);
+uint16_t irq_read_reg(unsigned char);
+void irq_unmask(unsigned char);
+void irq_unmask_all(void);
+void irq_mask_all(void);
+
+#endif
