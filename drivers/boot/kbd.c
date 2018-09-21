978fae4788911992e1ea2fbb382093151489eb7b
Yggdrasill <yggdrasill@shitpost.is>
2018-09-21 14:17:20 +0200

    Scan code->key code tables, relevant input handler
            This commit adds lookup tables for scan code to key code
            translation. It also implements a basic state machine for the
            keyboard driver, although no keyboard input buffer has been
            implemented yet.
    
            There are still some extended keys that aren't handled by the
            driver right now, notably the print screen key, which has a
            multi-byte scan code of 0xE0 0x12 0xE0 0x7C. Multimedia keys and
            ACPI-related keys are also not supported, only represented as
            0xFE and 0xFF for the moment.

--- drivers/boot/kbd.c
+++ drivers/boot/kbd.c
@@ -23,6 +23,80 @@
 #include "string.h"
 #include "kbd.h"
 
+static int ext;
+static int brk;
+
+const unsigned char kbd_scan_table[] = {
+  0x00,         KCODE_F9,     0x00,         KCODE_F5,     /* 0x00 - 0x03 */
+  KCODE_F3,     KCODE_F1,     KCODE_F2,     KCODE_F12,    /* 0x04 - 0x07 */
+  0x00,         KCODE_F10,    KCODE_F8,     KCODE_F6,     /* 0x08 - 0x0B */
+  KCODE_F4,     KCODE_TAB,    KCODE_TILDE,  0x00,         /* 0x0C - 0x0F */
+  0x00,         KCODE_LALT,   KCODE_LSHIFT, 0x00,         /* 0x10 - 0x13 */
+  KCODE_LCTRL,  KCODE_Q,      KCODE_ONE,    0x00,         /* 0x14 - 0x17 */
+  0x00,         0x00,         KCODE_Z,      KCODE_S,      /* 0x18 - 0x1B */
+  KCODE_A,      KCODE_W,      KCODE_TWO,    0x00,         /* 0x1C - 0x1F */
+  0x00,         KCODE_C,      KCODE_X,      KCODE_D,      /* 0x20 - 0x23 */
+  KCODE_E,      KCODE_FOUR,   KCODE_THREE,  0x00,         /* 0x24 - 0x27 */
+  0x00,         KCODE_SPACE,  KCODE_V,      KCODE_F,      /* 0x28 - 0x2B */
+  KCODE_T,      KCODE_R,      KCODE_FIVE,   0x00,         /* 0x2C - 0x2F */
+  0x00,         KCODE_N,      KCODE_B,      KCODE_H,      /* 0x30 - 0x33 */
+  KCODE_G,      KCODE_Y,      KCODE_SIX,    0x00,         /* 0x34 - 0x37 */
+  0x00,         0x00,         KCODE_M,      KCODE_J,      /* 0x38 - 0x3B */
+  KCODE_U,      KCODE_SEVEN,  KCODE_EIGHT,  0x00,         /* 0x3C - 0x3F */
+  0x00,         KCODE_COMMA,  KCODE_K,      KCODE_I,      /* 0x40 - 0x43 */
+  KCODE_O,      KCODE_ZERO,   KCODE_NINE,   0x00,         /* 0x44 - 0x47 */
+  0x00,         KCODE_DOT,    KCODE_FSLASH, KCODE_L,      /* 0x48 - 0x4B */
+  KCODE_SCOLON, KCODE_P,      KCODE_DASH,   0x00,         /* 0x4C - 0x4F */
+  0x00,         0x00,         KCODE_APO,    0x00,         /* 0x50 - 0x53 */
+  KCODE_LSQBRK, KCODE_EQUAL,  0x00,         0x00,         /* 0x54 - 0x57 */
+  KCODE_CAPS,   KCODE_RSHIFT, KCODE_RET,    KCODE_RSQBRK, /* 0x58 - 0x5B */
+  0x00,         KCODE_BSLASH, 0x00,         0x00,         /* 0x5C - 0x5F */
+  0x00,         0x00,         0x00,         0x00,         /* 0x60 - 0x63 */
+  0x00,         0x00,         KCODE_BCKSP,  0x00,         /* 0x64 - 0x67 */
+  0x00,         KCODE_NONE,   0x00,         KCODE_NFOUR,  /* 0x68 - 0x6B */
+  KCODE_NSEVEN, 0x00,         0x00,         0x00,         /* 0x6C - 0x6F */
+  KCODE_NZERO,  KCODE_NPOINT, KCODE_NTWO,   KCODE_NFIVE,  /* 0x70 - 0x73 */
+  KCODE_NSIX,   KCODE_NEIGHT, KCODE_ESC,    KCODE_NLOCK,  /* 0x74 - 0x77 */
+  KCODE_F11,    KCODE_NADD,   KCODE_NTHREE, KCODE_NSUB,   /* 0x78 - 0x7B */
+  KCODE_NMUL,   KCODE_NNINE,  KCODE_SCRLK,  0x00,         /* 0x7C - 0x7F */
+  0x00,         0x00,         0x00,         KCODE_F7      /* 0x80 - 0x83 */
+};
+
+const unsigned char kbd_ext_scan_table[] = {
+  0x00,         0x00,         0x00,         0x00,         /* 0x00 - 0x03 */
+  0x00,         0x00,         0x00,         0x00,         /* 0x04 - 0x07 */
+  0x00,         0x00,         0x00,         0x00,         /* 0x08 - 0x0B */
+  0x00,         0x00,         0x00,         0x00,         /* 0x0C - 0x0F */
+  KCODE_MULTI,  KCODE_RALT,   0x00,         0x00,         /* 0x10 - 0x13 */
+  KCODE_RCTRL,  KCODE_MULTI,  0x00,         0x00,         /* 0x14 - 0x17 */
+  KCODE_MULTI,  0x00,         0x00,         0x00,         /* 0x18 - 0x1B */
+  0x00,         0x00,         0x00,         KCODE_LMOD,   /* 0x1C - 0x1F */
+  KCODE_MULTI,  KCODE_MULTI,  0x00,         KCODE_MULTI,  /* 0x20 - 0x23 */
+  0x00,         0x00,         0x00,         KCODE_RMOD,   /* 0x24 - 0x27 */
+  KCODE_MULTI,  0x00,         0x00,         KCODE_MULTI,  /* 0x28 - 0x2B */
+  0x00,         0x00,         0x00,         KCODE_MENU,   /* 0x2C - 0x2F */
+  KCODE_MULTI,  0x00,         KCODE_MULTI,  0x00,         /* 0x30 - 0x33 */
+  KCODE_MULTI,  0x00,         0x00,         KCODE_ACPI,   /* 0x34 - 0x37 */
+  KCODE_MULTI,  0x00,         KCODE_MULTI,  KCODE_MULTI,  /* 0x38 - 0x3B */
+  0x00,         0x00,         0x00,         KCODE_ACPI,   /* 0x3C - 0x3F */
+  KCODE_MULTI,  0x00,         0x00,         0x00,         /* 0x40 - 0x43 */
+  KCODE_MULTI,  0x00,         0x00,         0x00,         /* 0x44 - 0x47 */
+  KCODE_MULTI,  0x00,         KCODE_NDIV,   0x00,         /* 0x48 - 0x4B */
+  0x00,         KCODE_MULTI,  0x00,         0x00,         /* 0x4C - 0x4F */
+  KCODE_MULTI,  0x00,         0x00,         0x00,         /* 0x50 - 0x53 */
+  0x00,         0x00,         0x00,         0x00,         /* 0x54 - 0x57 */
+  0x00,         0x00,         KCODE_NRET,   0x00,         /* 0x58 - 0x5B */
+  0x00,         0x00,         KCODE_ACPI,   0x00,         /* 0x5C - 0x5F */
+  0x00,         0x00,         0x00,         0x00,         /* 0x60 - 0x63 */
+  0x00,         0x00,         0x00,         0x00,         /* 0x64 - 0x67 */
+  0x00,         KCODE_END,    0x00,         KCODE_LARROW, /* 0x68 - 0x6B */
+  KCODE_HOME,   0x00,         0x00,         0x00,         /* 0x6C - 0x6F */
+  KCODE_INS,    KCODE_DEL,    KCODE_DARROW, 0x00,         /* 0x70 - 0x73 */
+  0x00,         0x00,         0x00,         0x00,         /* 0x74 - 0x77 */
+  0x00,         0x00,         KCODE_PGDN,   0x00,         /* 0x78 - 0x7B */
+  0x00,         KCODE_PGUP,   0x00,         0x00          /* 0x7C - 0x7F */
+};
+
 void kbd_write_wait(void)
 {
   unsigned char kbd;
@@ -55,15 +129,58 @@ void kbd_enable(void)
   outb(0x64, 0xA8);
 }
 
+void kbd_break(void)
+{
+  inb(0x60);
+  puts("Release");
+
+  brk = 0;
+  return;
+}
+
+void kbd_ext_input(void)
+{
+  unsigned char ch;
+
+  if(!brk) {
+    ch = inb(0x60);
+    switch(ch) {
+      case 0xF0:
+        brk = 1;
+        break;
+      default:
+        puthex(kbd_ext_scan_table[ch]);
+    }
+  } else {
+    kbd_break();
+  }
+
+  ext = 0;
+  return;
+}
+
 void kbd_input(void)
 {
   unsigned char kbd;
-  unsigned int ch;
+  unsigned char ch;
 
   kbd = inb(0x64);
-  if(!(kbd & 0x20) ) {
+  if(kbd & 0x01 && !ext && !brk) {
     ch = inb(0x60);
-    puthex(ch);
+    switch(ch) {
+      case 0xE0:
+        ext = 1;
+        break;
+      case 0xF0:
+        brk = 1;
+        break;
+      default:
+        puthex(kbd_scan_table[ch]);
+    }
+  } else if(ext) {
+    kbd_ext_input();
+  } else if(brk) {
+    kbd_break();
   }
 
   return;
