39c1d55797aca39df8ba9ae0ed429ca404e1d5a2
Yggdrasill <yggdrasill@shitpost.is>
2026-07-07 11:01:36 +0100

    Add GDT initialisation code
            This commit adds the following two files:
                    libk/gdt.h
                    libk/gdt.c
    
            The relevant data structures can be found in gdt.h, where
            helpful enums for GDT entry flag configuration are found.
    
            The following functions are introduced:
                    struct gdt_ptr *gdt_init(void)
                    void gdt_entry_add(
                            struct gdt_ptr  *gdtp,
                            void            *base,
                            uint32_t        limit,
                            uint8_t         access,
                            uint8_t         flags)
    
            gdt_init will set up a GDTR entry in memory at
            __GDT_PTR_LOCATION and zero out the first NULL GDT entry located
            at __GDT_BASE_LOCATION. It also sets up the basic GDTR
            structure, of course. A gdt_init_at() function will likely
            follow this one in time.
    
            gdt_entry_add will take all the necessary information to create
            a GDT entry, computing the new entry's location based on the
            size field of the GDTR in gdtp. The enums found in gdt.h should
            help with configuring the flags in the new entry, but this
            commit is not documentation for the GDT entries themselves. Such
            documentation can readily be found online.

---
+++ libk/gdt.h
@@ -0,0 +1,71 @@
+/*
+ * MBR bootloader, currently unnamed
+ * Copyright (C) 2017  Yggdrasill <kaymeerah@lambda.is>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ */
+
+#ifndef GDT_H
+#define GDT_H
+
+#include "stdint.h"
+
+enum GDT_ACCESS {
+    GDT_ACCESSED    = 1,
+    GDT_RW          = 1 << 1,
+    GDT_DC          = 1 << 2,
+    GDT_EXEC        = 1 << 3,
+    GDT_SEGMENT     = 1 << 4,
+    GDT_PRIV_RING0  = 0 << 5,
+    GDT_PRIV_RING1  = 1 << 5,
+    GDT_PRIV_RING2  = 2 << 5,
+    GDT_PRIV_RING3  = 3 << 5,
+    GDT_PRESENT     = 1 << 7
+};
+
+enum GDT_FLAGS {
+    GDT_RESERVED    = 0,
+    GDT_LONG        = 1 << 1,
+    GDT_BITS_16     = 0 << 2,
+    GDT_BITS_32     = 1 << 2,
+    GDT_GRAN        = 1 << 3
+};
+
+struct gdt_ptr {
+    uint8_t size_0;
+    uint8_t size_8;
+    uint8_t base_0;
+    uint8_t base_8;
+    uint8_t base_16;
+    uint8_t base_24;
+};
+
+struct gdt_entry {
+    uint8_t limit_0;
+    uint8_t limit_8;
+    uint8_t base_0;
+    uint8_t base_8;
+    uint8_t base_16;
+    uint8_t access;
+    uint8_t limit_flags;
+    uint8_t base_24;
+};
+
+extern struct gdt_ptr   __GDT_PTR_LOCATION;
+extern struct gdt_entry __GDT_BASE_LOCATION;
+
+#endif
