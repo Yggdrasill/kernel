680494b6988f51f822bb4ddd5566addbedb906b9
Yggdrasill <yggdrasill@shitpost.is>
2026-07-13 13:19:12 +0100

    start.s: refuse stage 2 if in 16-bit exec

--- boot/stage2/start.s
+++ boot/stage2/start.s
@@ -1,23 +1,35 @@
-bits 32
+extern __STACK_BASE_LOCATION
+
+extern __bios_error
 
 section ._init alloc exec progbits nowrite
 global __start
 
 __start:
-	push  ebp
-	mov   ebp, esp
-
-	mov   esp, 0x7FFF0
-	mov   ebp, 0x7FFF0
-	
+bits 16
+	xor    ax, ax
+	inc    ax
+	shl    ax, 17
+	bt     ax, 17
+	jc     continue_32
+	push   dword exec_len
+	push   dword exec_err
+	call   __bios_error
+bits 32
+continue_32:
+	mov   esp, __STACK_BASE_LOCATION
+	mov   ebp, __STACK_BASE_LOCATION
 	call  main
-
 	; if we ever get to this point, 
 	; just give up, something went
 	; wrong.
-
 	cli
+hltlp:
 	hlt
+	jmp   hltlp
+
+exec_err db "E: Cannot execute stage 2 in 16-bit mode!",0x0D,0x0A
+exec_len equ $ - exec_err
 
 section .text
 extern main
