53152e182d7cdf8ed002bec74feb800fde04712f
Yggdrasill <yggdrasill@shitpost.is>
2026-07-18 12:53:53 +0100

    build/linker: generate scripts with C preprocessor
    	This commit changes the build system to generate linker scripts
    	using the C preprocessor. The reason for this is that I may now
    	actually use C defined constants in my linker script, which
    	allows me to not rely on __attribute__ at all.

--- boot/stage1/Rules.mk
+++ boot/stage1/Rules.mk
@@ -1,5 +1,8 @@
-LD_STAGE1=-T $(SRCDIR_STAGE1)/linker.ld
+LD_STAGE1=-T $(SRCDIR_STAGE1)/linker.ld -T $(SRCDIR_BOOT_COMMON)/s1_bootdefs.lds
 AF_BOOT=-f elf32 -I $(SRCDIR_STAGE1)/
+
+$(SRCDIR_BOOT_COMMON)/s1_bootdefs.lds: $(SRCDIR_BOOT_COMMON)/bootdefs.lds.S
+	$(CC) $(INCLUDE_PATH) -DLD_BOOT_STAGE1 -E -P -x c -o $@ $^
 
 $(OBJDIR_STAGE1)/%.o: $(SRCDIR_STAGE1)/%.s
 	$(AS) $(AF_BOOT) -o $@ $^
@@ -7,5 +10,5 @@ $(OBJDIR_STAGE1)/%.o: $(SRCDIR_STAGE1)/%.s
 $(OBJDIR_STAGE1)/%.o: $(SRCDIR_BOOT_COMMON)/%.s
 	$(AS) $(AF_BOOT) -o $@ $^
 
-$(BINDIR)/boot.bin: $(OBJ_STAGE1) $(OBJ_COMMON)
-	$(LD) $(LD_ALL) $(LD_BOOT) $(LD_STAGE1) -o $@ $^
+$(BINDIR)/boot.bin: $(OBJ_STAGE1) $(OBJ_COMMON) $(SRCDIR_BOOT_COMMON)/s1_bootdefs.lds
+	$(LD) $(LD_ALL) $(LD_BOOT) $(LD_STAGE1) -o $@ $(OBJ_STAGE1) $(OBJ_COMMON)
