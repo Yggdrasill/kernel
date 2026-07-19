f4e10897126763da8eb23e9050b8c6a72a9ace9b
Yggdrasill <yggdrasill@shitpost.is>
2026-07-19 16:41:12 +0100

    Makefile typo fixes, dependency improvements
    Make mmap.o depend on $(OBJDIR_STAGE2)

--- boot/stage1/Rules.mk
+++ boot/stage1/Rules.mk
@@ -1,7 +1,7 @@
 LD_STAGE1=-T $(OBJDIR_STAGE1)/linker.lds
 AF_BOOT=-f elf32 -I $(SRCDIR_STAGE1)/
 
-$(OBJDIR_STAGE1)/linker.lds: $(SRCDIR_BOOT_COMMON)/bootdefs.lds.S
+$(OBJDIR_STAGE1)/linker.lds: $(SRCDIR_BOOT_COMMON)/bootdefs.lds.S | $(OBJDIR_STAGE1)
 	$(CC) -MMD -MP -MF $@.d -MT $@ $(INCLUDE_PATH) -DLD_BOOT_STAGE1 -E -P -x c -o $@ $<
 
 $(OBJDIR_STAGE1)/%.o: $(SRCDIR_STAGE1)/%.s | $(OBJDIR_STAGE1)
@@ -10,5 +10,5 @@ $(OBJDIR_STAGE1)/%.o: $(SRCDIR_STAGE1)/%.s | $(OBJDIR_STAGE1)
 $(OBJDIR_STAGE1)/%.o: $(SRCDIR_BOOT_COMMON)/%.s | $(OBJDIR_STAGE1)
 	$(AS) $(AF_BOOT) -o $@ $<
 
-$(BINDIR)/boot.bin: $(OBJ_STAGE1) $(OBJ_COMMON) | $(BINDIR) $(OBJDIR_STAGE1)/linker.lds
+$(BINDIR)/boot.bin: $(OBJ_STAGE1) $(OBJ_COMMON) $(OBJDIR_STAGE1)/linker.lds | $(BINDIR)
 	$(LD) $(LD_ALL) $(LD_BOOT) $(LD_STAGE1) -o $@ $(OBJ_STAGE1) $(OBJ_COMMON)
--- boot/stage2/Rules.mk
+++ boot/stage2/Rules.mk
@@ -17,10 +17,12 @@ $(OBJDIR_GEN)/linker_gen: $(SRCDIR_STAGE2)/linker_gen.c | $(OBJDIR_GEN)
 $(OBJDIR_GEN)/mmap_generated.h: $(OBJDIR_GEN)/linker_gen
 	$< > $@
 
-$(OBJDIR_STAGE2)/linker.lds: $(SRCDIR_BOOT_COMMON)/bootdefs.lds.S | $(OBJDIR_STAGE2) $(OBJDIR_GEN)/mmap_generated.h
+$(OBJDIR_STAGE2)/linker.lds: $(SRCDIR_BOOT_COMMON)/bootdefs.lds.S \
+							 $(OBJDIR_GEN)/mmap_generated.h | $(OBJDIR_STAGE2)
 	$(CC) -MMD -MP -MF $@.d -MT $@ $(INCLUDE_PATH) -DLD_BOOT_STAGE2 -E -P -x c -o $@ $<
 
-$(OBJDIR_STAGE2)/mmap.o: $(SRCDIR_STAGE2)/mmap.s | $(OBJDIR_GEN)/mmap_generated.s
+$(OBJDIR_STAGE2)/mmap.o: $(SRCDIR_STAGE2)/mmap.s $(OBJDIR_GEN)/mmap_generated.s \
+						 | $(OBJDIR_STAGE2)
 	$(AS) -f elf32 -i $(OBJDIR_GEN) -o $@ $<
 
 $(OBJDIR_STAGE2)/%.o: $(SRCDIR_STAGE2)/%.s | $(OBJDIR_STAGE2)
@@ -29,5 +31,6 @@ $(OBJDIR_STAGE2)/%.o: $(SRCDIR_STAGE2)/%.s | $(OBJDIR_STAGE2)
 $(OBJDIR_STAGE2)/%.o: $(SRCDIR_STAGE2)/%.c | $(OBJDIR_STAGE2)
 	$(CC) $(CF_DEP) $(CF_ALL) $(INCLUDE_PATH) -I $(SRCDIR_STAGE2)/ $(CFLAGS) -c -o $@ $<
 
-$(BINDIR)/stage2.elf: $(OBJDIR_STAGE2)/linker.lds $(OBJDIR)/libk.o $(OBJDIR)/klibc.o $(OBJ_STAGE2) $(OBJ_STAGE1) $(OBJ_COMMON) | $(BINDIR) $(OBJDIR_STAGE2)/linker.lds
+$(BINDIR)/stage2.elf: $(OBJDIR_STAGE2)/linker.lds $(OBJDIR)/libk.o $(OBJDIR)/klibc.o \
+					  $(OBJ_STAGE2) $(OBJ_STAGE1) $(OBJ_COMMON) | $(BINDIR)
 	$(LD) $(LD_ALL) $(LD_BOOT) $(LD_STAGE2) -o $@ $(filter-out $(OBJDIR_STAGE2)/linker.lds,$^)
