dd3dad5ab618d840808357d47f53b992f0aa2f75
Yggdrasill <yggdrasill@shitpost.is>
2026-07-20 14:33:29 +0100

    License headers

--- boot/stage2/symbol_gen.c
+++ boot/stage2/symbol_gen.c
@@ -1,12 +1,29 @@
+/*
+ * MBR bootloader, currently unnamed
+ * Copyright (C) 2017  Yggdrasill <kaymeerah@lambda.is>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ */
+
 #include <libk/internal/mmap.h>
 #include <stddef.h>
 #include <stdint.h>
 
-/*
- * Unfortunately this process assumes that the computer building this is at
- * least compatible with the build target. I will fix this problem in time, but
- * for now I need compile-time generation of sizes.
- */
+/* Export symbols to ELF file, to be used in assembly and linker scripts. */
 
 volatile const uint32_t ABI_MMAP_TABLE_SIZE = MMAP_TABLE_SIZE;
 volatile const uint32_t ABI_MMAP_ENTRY_SIZE = MMAP_ENTRY_SIZE;
