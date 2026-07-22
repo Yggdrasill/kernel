10bac96e1eacec154c110e4fae54d117d4a7a8f2
Yggdrasill <yggdrasill@shitpost.is>
2026-07-22 14:27:55 +0100

    build: stage 2 build cleanup, prepare local mmap.c

--- boot/stage2/Rules.mk
+++ boot/stage2/Rules.mk
@@ -1,7 +1,6 @@
 SRC_STAGE2:=$(filter-out $(SRCDIR_STAGE2)/symbol_gen.c,$(wildcard $(SRCDIR_STAGE2)/*.c))
-SRC_STAGE2:=$(SRC_STAGE2) \
-			$(filter-out $(SRCDIR_STAGE2)/mmap_generated.s,$(wildcard $(SRCDIR_STAGE2)/*.s))
-OBJ_STAGE2=$(patsubst %.s,%.o,$(patsubst %.c,%.o,\
+SRC_STAGE2:=$(SRC_STAGE2) $(wildcard $(SRCDIR_STAGE2)/*.s)
+OBJ_STAGE2=$(patsubst %.s,%_asm.o,$(patsubst %.c,%.o,\
 		   $(patsubst $(SRCDIR_STAGE2)%,$(OBJDIR_STAGE2)%,$(SRC_STAGE2))))
 
 LD_STAGE2=-T $(OBJDIR_STAGE2)/linker.lds
@@ -38,16 +37,17 @@ $(OBJDIR_STAGE2)/linker.lds: $(SRCDIR_BOOT_COMMON)/linker.lds.S \
 							 $(OBJDIR_GEN)/mmap_generated.h | $(OBJDIR_STAGE2)
 	$(CC) -MMD -MP -MF $@.d -MT $@ $(INCLUDE_PATH) -DLD_BOOT_STAGE2 -E -P -x c -o $@ $<
 
-$(OBJDIR_STAGE2)/mmap.o: $(SRCDIR_STAGE2)/mmap.s $(OBJDIR_GEN)/mmap_generated.s \
-						 | $(OBJDIR_STAGE2)
+$(OBJDIR_STAGE2)/mmap_asm.o: $(SRCDIR_STAGE2)/mmap.s $(OBJDIR_GEN)/mmap_generated.s \
+						     | $(OBJDIR_STAGE2)
 	$(AS) -f elf32 -i $(OBJDIR_GEN) -o $@ $<
 
-$(OBJDIR_STAGE2)/%.o: $(SRCDIR_STAGE2)/%.s | $(OBJDIR_STAGE2)
+$(OBJDIR_STAGE2)/%_asm.o: $(SRCDIR_STAGE2)/%.s | $(OBJDIR_STAGE2)
 	$(AS) -f elf32 -o $@ $<
 
 $(OBJDIR_STAGE2)/%.o: $(SRCDIR_STAGE2)/%.c | $(OBJDIR_STAGE2)
 	$(CC) $(CF_DEP) $(CF_ALL) $(INCLUDE_PATH) -I $(SRCDIR_STAGE2)/ $(CFLAGS) -c -o $@ $<
 
-$(BINDIR)/stage2.elf: $(OBJDIR_STAGE2)/linker.lds $(OBJDIR)/libk.o $(OBJDIR)/klibc.o \
-					  $(OBJ_STAGE2) $(OBJ_STAGE1) $(OBJ_COMMON) | $(BINDIR)
+$(BINDIR)/stage2.elf: $(OBJDIR_STAGE2)/linker.lds $(OBJDIR)/libk.o \
+					  $(OBJDIR)/klibc.o $(OBJ_STAGE2) $(OBJ_STAGE1) \
+					  $(OBJ_COMMON) | $(BINDIR)
 	$(LD) $(LD_ALL) $(LD_BOOT) $(LD_STAGE2) -o $@ $(filter-out $(OBJDIR_STAGE2)/linker.lds,$^)
